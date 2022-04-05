71f14512c93313fb29f874850bb516a23a2e6479
rupair2022 <102897383+rupair2022@users.noreply.github.com>
2022-04-05 15:34:19 +0530

    Add files via upload

---
+++ 9xmlparsing.py
@@ -0,0 +1,77 @@
+import csv 
+import requests 
+import xml.etree.ElementTree as ET 
+
+def loadRSS():
+    # url of rss feed 
+    url = 'https://www.w3schools.com/xml/simple.xml'
+
+    # creating HTTP response object from given url 
+    resp = requests.get(url) 
+
+    # saving the xml file 
+    with open('topnewsfeed.xml', 'wb') as f: 
+        f.write(resp.content) 
+        
+
+def parseXML(xmlfile): 
+
+    # create element tree object 
+    tree = ET.parse(xmlfile) 
+
+    # get root element 
+    root = tree.getroot() 
+
+    # create empty list for news items 
+    newsitems = [] 
+
+    # iterate child elements of item 
+    for item in root.findall('./food'):
+  
+        # empty news dictionary
+        news = {}
+  
+        # iterate child elements of item
+        for child in item:
+            news[child.tag] = child.text.encode('utf8')
+  
+        # append news dictionary to news items list
+        newsitems.append(news)
+      
+    # return news items list
+    return newsitems
+
+
+def savetoCSV(newsitems, filename): 
+
+    # specifying the fields for csv file 
+    fields = ['name','price', 'description', 'calories'] 
+
+    # writing to csv file 
+    with open(filename, 'w') as csvfile: 
+
+        # creating a csv dict writer object 
+        writer = csv.DictWriter(csvfile, fieldnames = fields) 
+
+        # writing headers (field names) 
+        writer.writeheader() 
+
+        # writing data rows 
+        writer.writerows(newsitems) 
+
+    
+def main(): 
+    # load rss from web to update existing xml file 
+    loadRSS() 
+
+    # parse xml file 
+    newsitems = parseXML('topnewsfeed.xml') 
+    print(newsitems)
+    # store news items in a csv file 
+    savetoCSV(newsitems, 'topnews.csv') 
+    
+    
+if __name__ == "__main__": 
+
+    # calling main function 
+    main() 
